2222d53a35d29d6249840ca55d04fe975beedff6
fumiya konno <konno@ics-web.jp>
2019-12-25 18:17:16 +0900

    ml5.js記事のデモ

---
+++ FacaApi_Star_Demo/sketch.js
@@ -0,0 +1,171 @@
+let faceapi;
+let video;
+let detections;
+let stars = []
+let starSize = 10
+
+// by default all options are set to true
+const detection_options = {
+    withLandmarks: true,
+    withDescriptors: false,
+}
+
+
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+
+    // load up your video
+    video = createCapture(VIDEO);
+    video.size(width, height);
+    video.hide(); // Hide the video element, and just show the canvas
+    faceapi = ml5.faceApi(video, detection_options, modelReady)
+    textAlign(RIGHT);
+}
+
+function modelReady() {
+    console.log('ready!')
+    console.log(faceapi)
+    faceapi.detect(gotResults)
+
+}
+
+function gotResults(err, result) {
+    if (err) {
+        console.log(err)
+        return
+    }
+    // console.log(result)
+    detections = result;
+
+    // background(220);
+    background(255);
+    image(video, 0, 0, width, height)
+    if (detections) {
+        if (detections.length > 0) {
+            // console.log(detections)
+            drawBox(detections)
+            drawLandmarks(detections)
+            // 顔の面積を取得します
+            setFaceArea(detections)
+        }
+
+    }
+    faceapi.detect(gotResults)
+}
+
+function drawBox(detections) {
+    for (let i = 0; i < detections.length; i++) {
+        const alignedRect = detections[i].alignedRect;
+        const x = alignedRect._box._x
+        const y = alignedRect._box._y
+        const boxWidth = alignedRect._box._width
+        const boxHeight = alignedRect._box._height
+
+        noFill();
+        stroke(161, 95, 251);
+        strokeWeight(2);
+        rect(x, y, boxWidth, boxHeight);
+    }
+
+}
+
+function drawLandmarks(detections) {
+    noFill();
+    stroke(161, 95, 251)
+    strokeWeight(2)
+
+    for (let i = 0; i < detections.length; i++) {
+        // const mouth = detections[i].parts.mouth;
+        // const nose = detections[i].parts.nose;
+        const leftEye = detections[i].parts.leftEye;
+        const rightEye = detections[i].parts.rightEye;
+        // const rightEyeBrow = detections[i].parts.rightEyeBrow;
+        // const leftEyeBrow = detections[i].parts.leftEyeBrow;
+
+        // drawPart(mouth, true);
+        // drawPart(nose, false);
+        // drawPart(leftEye, true);
+        // drawPart(leftEyeBrow, false);
+        // drawPart(rightEye, true);
+        // drawPart(rightEyeBrow, false);
+        drawStar(leftEye, "L")
+        drawStar(rightEye, "R")
+    }
+
+}
+
+// 星を作成するクラスです
+// 参考：p5.js example https://p5js.org/examples/form-star.html
+class Star{
+    constructor(x, y, radius1, radius2, npoints, LR) {
+        this.x = x
+        this.y = y
+        // 星を打ち上げる速度をランダムに決定
+        this.vy = (Math.random() + 1) * -10
+        this.radius1 = radius1
+        this.radius2 = radius2
+        this.npoints = npoints
+        // 星の色をランダムに決定
+        this.color= color(Math.random()*255,Math.random()*255,Math.random()*255)
+        // 逆の目の方向に星が飛ばないように
+        if(LR === "L"){
+            this.angleX = Math.random() * -10
+        }else {
+            this.angleX = Math.random() * 10
+        }
+    }
+    draw(){
+        let angle = TWO_PI / this.npoints;
+        let halfAngle = angle / 2.0;
+        fill(this.color)
+        noStroke()
+        // 星のシェイプを描画
+        beginShape();
+        for (let a = 0; a < TWO_PI; a += angle) {
+            let sx = this.x + cos(a) * this.radius2;
+            let sy = this.y + sin(a) * this.radius2;
+            vertex(sx, sy);
+            sx = this.x + cos(a + halfAngle) * this.radius1;
+            sy = this.y + sin(a + halfAngle) * this.radius1;
+            vertex(sx, sy);
+        }
+        endShape(CLOSE);
+
+        // 星を回転させながら落とす
+        this.vy +=  3
+        this.y += this.vy
+        this.x += this.angleX
+    }
+}
+
+// 目の中心からStarを発生させます
+function drawStar(eye, LR) {
+    const center = getCenter(eye)
+    const star = new Star(center.avgX, center.avgY, starSize / 3, starSize, 5, LR)
+    stars.push(star)
+    stars.forEach(star => {
+        star.draw()
+    })
+    if(stars.length > 100) stars.shift()
+}
+
+// 顔の面積を取得します
+function setFaceArea(detections) {
+    for (let i = 0; i < detections.length; i++) {
+        const alignedRect = detections[i].alignedRect;
+        const boxWidth = alignedRect._box._width
+        const boxHeight = alignedRect._box._height
+        starSize = boxHeight / 10
+    }
+}
+
+// 複数の座標の中心を取得する関数です
+function getCenter(arr) {
+    const sumX = arr.reduce((sum, item) => sum + item._x , 0)
+    const sumY = arr.reduce((sum, item) =>  sum + item._y , 0)
+    const avgX = sumX / arr.length
+    const avgY = sumY / arr.length
+    return {avgX, avgY}
+}
+
+
---
+++ posenet_Demo_Kanikoro/sketch.js
@@ -0,0 +1,120 @@
+// Copyright (c) 2019 ml5
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+/* ===
+ml5 Example
+PoseNet example using p5.js
+=== */
+
+let video;
+let poseNet;
+let poses = [];
+let kanikoro
+
+function setup() {
+  createCanvas(640, 480);
+  video = createCapture(VIDEO);
+  video.size(width, height);
+
+  kanikoro = loadImage("./kanikoro.png")
+
+  // Create a new poseNet method with a single detection
+  poseNet = ml5.poseNet(video, modelReady);
+  // This sets up an event that fills the global variable "poses"
+  // with an array every time new poses are detected
+  poseNet.on('pose', function(results) {
+    poses = results;
+  });
+  // Hide the video element, and just show the canvas
+  video.hide();
+}
+
+function modelReady() {
+  select('#status').html('Model Loaded');
+}
+
+function draw() {
+  image(video, 0, 0, width, height);
+
+  // We can call both functions to draw all keypoints and the skeletons
+  drawKeypoints();
+  drawSkeleton();
+  drawKanikoroR()
+  drawKanikoroL()
+}
+
+// A function to draw ellipses over the detected keypoints
+function drawKeypoints()  {
+  // Loop through all the poses detected
+  for (let i = 0; i < poses.length; i++) {
+    // For each pose detected, loop through all the keypoints
+    let pose = poses[i].pose;
+    for (let j = 0; j < pose.keypoints.length; j++) {
+      // A keypoint is an object describing a body part (like rightArm or leftShoulder)
+      let keypoint = pose.keypoints[j];
+      // Only draw an ellipse is the pose probability is bigger than 0.2
+      if (keypoint.score > 0.2) {
+        fill(255, 0, 0);
+        noStroke();
+        ellipse(keypoint.position.x, keypoint.position.y, 10, 10);
+      }
+    }
+  }
+}
+
+// A function to draw the skeletons
+function drawSkeleton() {
+  // Loop through all the skeletons detected
+  for (let i = 0; i < poses.length; i++) {
+    let skeleton = poses[i].skeleton;
+    // For every skeleton, loop through all body connections
+    for (let j = 0; j < skeleton.length; j++) {
+      let partA = skeleton[j][0];
+      let partB = skeleton[j][1];
+      stroke(255, 0, 0);
+      line(partA.position.x, partA.position.y, partB.position.x, partB.position.y);
+    }
+  }
+}
+
+// 右手のかにクリームコロッケ
+function drawKanikoroR() {
+  for(let i = 0; i < poses.length; i++) {
+    const rightWrist = poses[i].pose.rightWrist
+    const rightWristVector = createVector(rightWrist.x, rightWrist.y)
+    const rightElbow = poses[i].pose.rightElbow
+    const rightElbowVector = createVector(rightElbow.x, rightElbow.y)
+    const vy = createVector(0, 1)
+
+    const rightHandVector = createVector(rightWristVector.x - rightElbowVector.x, rightWristVector.y - rightElbowVector.y)
+    let rightAngle = rightHandVector.angleBetween(vy)
+    push()
+    translate(rightWrist.x + rightHandVector.x /10, rightWrist.y + rightHandVector.y/10)
+    rotate(Math.PI + rightAngle)
+    image(kanikoro, -75, -150, 150, 250)
+    pop()
+  }
+}
+
+// 左手のかにクリームコロッケ
+function drawKanikoroL() {
+  for(let i = 0; i < poses.length; i++) {
+
+    const leftWrist = poses[i].pose.leftWrist
+    const leftWristVector = createVector(leftWrist.x, leftWrist.y)
+    const leftElbow = poses[i].pose.leftElbow
+    const leftElbowVector = createVector(leftElbow.x, leftElbow.y)
+    const vy = createVector(0, 1)
+
+    const leftHandVector = createVector(leftWristVector.x - leftElbowVector.x, leftWristVector.y - leftElbowVector.y)
+    let leftAngle = leftHandVector.angleBetween(vy)
+
+    push()
+    translate(leftWrist.x, leftWrist.y)
+    rotate(Math.PI + leftAngle)
+    image(kanikoro, -75, -150, 150, 250)
+    pop()
+  }
+}
